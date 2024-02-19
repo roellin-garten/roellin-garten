479cfa1e18338ba2d00221572699a2fed15150a9
thormeier <thormeier@users.noreply.github.com>
2024-02-19 17:12:11 +0000

    deploy: fdc5a0cacbbdf93b32d87cbbfab4c83d66edaeae

--- _nuxt/FullScreenSection.B37iOA4W.js
+++
@@ -1,1 +0,0 @@
-import{_ as n,o,c as s,x as c}from"./entry.WeI6kZ6p.js";const t={},_={class:"min-h-screen w-full"};function r(e,a){return o(),s("section",_,[c(e.$slots,"default")])}const f=n(t,[["render",r]]);export{f as _};
