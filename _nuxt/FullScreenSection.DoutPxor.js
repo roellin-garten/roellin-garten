440b767a54b733d2944d147251cfe61dc7e5dc41
thormeier <thormeier@users.noreply.github.com>
2024-04-01 11:45:35 +0000

    deploy: a586238fe361007ab870be6157fff7db74e093fc

---
+++ _nuxt/FullScreenSection.DoutPxor.js
@@ -0,0 +1,1 @@
+import{_ as n,o,c as s,x as c}from"./entry.h7Rt-BL5.js";const t={},_={class:"min-h-screen w-full"};function r(e,a){return o(),s("section",_,[c(e.$slots,"default")])}const f=n(t,[["render",r]]);export{f as _};
